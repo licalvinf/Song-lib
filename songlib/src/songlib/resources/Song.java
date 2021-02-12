ebd62cfd6953a478fe92cf17120c627f95ee7a41
Calvin <Calvin@CalvinGame.local>
2021-02-12 17:12:59 -0500

    1st draft

---
+++ songlib/src/songlib/resources/Song.java
@@ -0,0 +1,15 @@
+package songlib.resources;
+
+public class Song {
+	String name;
+	String artist;
+	String year;
+	String album;
+	
+	public Song(String n, String art, String y, String alb) {
+		name = n;
+		artist = art;
+		year = y;
+		album = alb;		
+	}
+}
---
+++ songlib/src/songlib/view/SonglibController.java
@@ -0,0 +1,50 @@
+package songlib.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import songlib.resources.Song;
+
+public class SonglibController {
+	@FXML Button addb;
+	@FXML Button editb;
+	@FXML TextField nameadd;
+	@FXML TextField nameedit;
+	@FXML TextField artistadd;
+	@FXML TextField artistedit;
+	@FXML TextField yearadd;
+	@FXML TextField yearedit;
+	@FXML TextField albumadd;
+	@FXML TextField albumedit;
+	
+	public void addSong(ActionEvent e) {
+		Button b = (Button)e.getSource();
+		if(b == addb) {
+			String name = null; String artist=null; String year=null; String album=null;
+			if(nameadd.getText().isEmpty() || artistadd.getText().isEmpty() ) {
+				//Throw Error Message
+			}
+			else {
+				name = nameadd.getText();
+				artist = artistadd.getText();
+			}
+			if(yearadd.getText().isEmpty()) {
+				year = "<No Info>";
+			}
+			else {
+				year = yearadd.getText();
+			}
+			if(albumadd.getText().isEmpty()) {
+				album = "<No Info>";
+			}
+			else {
+				album = yearadd.getText();
+			}
+			Song addedSong = new Song(name, artist, year, album);			
+		}
+
+	}
+	
+}
