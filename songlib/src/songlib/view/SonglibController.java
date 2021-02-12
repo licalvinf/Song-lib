59a2bcd219802349ae736a165cb6cb26ebf3d262
Calvin <Calvin@CalvinGame.local>
2021-02-12 18:13:52 -0500

    updated Song resource, replaced fxml ids with fx:id, added more functionality to songlibcontroller(transferSong probably redundant)

--- songlib/src/songlib/view/SonglibController.java
+++ songlib/src/songlib/view/SonglibController.java
@@ -19,12 +19,18 @@ public class SonglibController {
 	@FXML TextField albumadd;
 	@FXML TextField albumedit;
 	
+	Song selectedSong = null;
+	
+	public void transferSong(Song s) {
+		selectedSong = s;
+	}
+	
 	public void addSong(ActionEvent e) {
 		Button b = (Button)e.getSource();
 		if(b == addb) {
 			String name = null; String artist=null; String year=null; String album=null;
 			if(nameadd.getText().isEmpty() || artistadd.getText().isEmpty() ) {
-				//Throw Error Message
+				//TOADD: Throw Error Message
 			}
 			else {
 				name = nameadd.getText();
@@ -47,14 +53,37 @@ public class SonglibController {
 			artistadd.setText("");
 			yearadd.setText("");
 			albumadd.setText("");
-			Song addedSong = new Song(name, artist, year, album);			
+			Song addedSong = new Song(name, artist, year, album);
+			//TOADD: Add song, check for existing song, reorder
 		}
 	}
 	public void editSong(ActionEvent e) {
 		Button b = (Button)e.getSource();
 		if(b == editb) {
-			//TOADD: Get song details from ListController
-			Song retrievedSong = null;
+			//TOADD: Get Song object from ListController
+			Song selectedSong = new Song("a","b","c","d");
+			if(!(nameedit.getText().isEmpty())) {
+				selectedSong.name = nameedit.getText();
+			}
+			if(!(artistedit.getText().isEmpty())) {
+				selectedSong.artist = artistedit.getText();
+			}
+			if(!(yearedit.getText().isEmpty())) {
+				selectedSong.year = yearedit.getText();
+			}
+			if(!(albumedit.getText().isEmpty())) {
+				selectedSong.album = albumedit.getText();
+			}
+			nameedit.setText("");
+			artistedit.setText("");
+			yearedit.setText("");
+			albumedit.setText("");
+			//TOADD: Check for existing song, reorder
 		}
+	}
+	public void deleteSong(ActionEvent e) {
+		Button b = (Button)e.GetSource();
+		
+		
 	}
 }
