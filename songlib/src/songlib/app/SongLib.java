9486dbc141797b35e8bb14813c8bb3eb0f2ed3e6
ac1792 <ac1792@DESKTOP-8HRIDRU>
2021-02-12 18:59:27 -0500

    Able to route selected ListView item to "Song Details" Section.

--- songlib/src/songlib/app/SongLib.java
+++ songlib/src/songlib/app/SongLib.java
@@ -17,7 +17,7 @@ public class SongLib extends Application {
 		
 		//Added listcontroller for the songView list -- Andrew 02/12/2021
 		SonglibController listCtrl = loader.getController();
-		listCtrl.startList();
+		listCtrl.startList(primaryStage);
 		
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
--- songlib/src/songlib/view/SonglibController.java
+++ songlib/src/songlib/view/SonglibController.java
@@ -6,7 +6,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import songlib.resources.Song;
 
 
@@ -22,6 +24,7 @@ public class SonglibController {
 	@FXML TextField albumadd;
 	@FXML TextField albumedit;
 	@FXML ListView<String> songView; //songView is the fx:id in .fxml file -- Andrew 02/12/2021
+	@FXML TextArea songDetails; //Song Details for selections
 	private ObservableList<String> songListObj; //-- Andrew 02/12/2021
 	
 	public void addSong(ActionEvent e) {
@@ -53,14 +56,30 @@ public class SonglibController {
 	}
 	
 	//-- Andrew 02/12/2021
-	public void startList() {
-		songListObj = FXCollections.observableArrayList("Song1", "Song2");
+	public void startList(Stage primaryStage) {
+		songListObj = FXCollections.observableArrayList("Song1\nArtist1\nAlbum1\n2000", "Song2\nArtist2\nAlbum2\n2002");
 		//System.out.println(songListObj);
 		try {
 			songView.setItems(songListObj);
 		} catch (Exception e) {
 			System.out.println(e);
 		}
+		
+		
+		songView.getSelectionModel().select(0); //Default selects first song
+		songView.getSelectionModel().selectedIndexProperty()
+		.addListener((obj, before, after) -> handleSelection(primaryStage));; //Set selection listener
 	}
 	
+	//Handles songView selection
+	private void handleSelection(Stage primaryStage) {
+		//Route songView selection to songDetails for display
+		String selectedSong = songView.getSelectionModel().getSelectedItem();
+		String[] songElements = selectedSong.split("\n");
+		String name = songElements[0]; String artist = songElements[1];
+		String album = songElements[2]; String year = songElements[3];
+		String outputDetails = String.format("Name:\t\t%s\nArtist:\t\t%s\nAlbum:\t\t%s\nYear:\t\t%s", name, artist, album, year);
+		//System.out.println(outputDetails);
+		songDetails.setText(outputDetails);
+	}
 }
